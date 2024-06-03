16f8651a3afd12ea0ebb405d62269d31674cc1c0
EdersonHTT <edersondasilvaschmidt@gmail.com>
2024-06-03 00:45:27 -0300

    atualizado

--- Teste.js
+++ Teste.js
@@ -4,7 +4,13 @@ let nome
 let idade
 let etinia
 let comprarMais
-
+let quantidades = {
+    macas: 0,
+    uvas: 0,
+    laranjas: 0,
+    peras: 0,
+    limoes: 0,
+}
 
 for (let x = 1; x <= y; x++){
 function consoleCarteira (){
@@ -75,67 +81,74 @@ function nacionalidade (nacional){
 nacionalidade(carteira.identidade.itinia)
 
 
+
 function mercadoDeFrutas (){
       
    let sacola1 = {
     fruta: "maça",
-    disponibilidade: true,
+    disponibilidade: 21,
     preco: 12.00
     }
+    if (y > 1){
+        sacola1.disponibilidade = sacola1.disponibilidade - quantidades.macas
+    }
 
-       let sacola2 = {
+    let sacola2 = {
     fruta: "uva",
-    disponibilidade: true,
+    disponibilidade: 23,
     preco: 6.00
     }
+    if (y > 1){
+        sacola2.disponibilidade = sacola2.disponibilidade - quantidades.uvas
+    }
 
-       let sacola3 = {
+    let sacola3 = {
     fruta: "laranja",
-    disponibilidade: true,
+    disponibilidade: 13,
     preco: 9.00
     }
-
-       let sacola4 = {
-    fruta: "pera",
-    disponibilidade: true,
-    preco: 5.00
+    if (y > 1){
+        sacola3.disponibilidade = sacola3.disponibilidade - quantidades.laranjas
     }
 
-       let sacola5 = {
+    let sacola4 = {
+    fruta: "pera",
+    disponibilidade: 24,
+    preco: 5.00
+    }
+    if (y > 1){
+        sacola4.disponibilidade = sacola4.disponibilidade - quantidades.peras
+    }
+
+    let sacola5 = {
     fruta: "limão",
-    disponibilidade: true,
+    disponibilidade: 12,
     preco: 7.50
+    }
+    if (y > 1){
+        sacola5.disponibilidade = sacola5.disponibilidade - quantidades.limoes
     }
 
     let paraSacolas = [sacola1, sacola2, sacola3, sacola4, sacola5]
     return paraSacolas
 }
-
 let sacolas = mercadoDeFrutas()
 
-sacolas[0].disponibilidade = false
-sacolas[1].disponibilidade = true
-sacolas[2].disponibilidade = true
-sacolas[3].disponibilidade = true
-sacolas[4].disponibilidade = true
-
-if (sacolas[0].disponibilidade === false) {
-    sacolas[0].fruta = 0
+if (sacolas[0].disponibilidade <= 0) {
+    sacolas[0].fruta = false
 }
-if (sacolas[1].disponibilidade === false) {
-    sacolas[1].fruta = 0
+if (sacolas[1].disponibilidade <= 0) {
+    sacolas[1].fruta = false
 }
-if (sacolas[2].disponibilidade === false) {
-    sacolas[2].fruta = 0
+if (sacolas[2].disponibilidade <= 0) {
+    sacolas[2].fruta = false
 }
-if (sacolas[3].disponibilidade === false) {
-    sacolas[3].fruta = 0
+if (sacolas[3].disponibilidade <= 0) { 
+    sacolas[3].fruta = false
 }
-if (sacolas[4].disponibilidade === false) {
-    sacolas[4].fruta = 0
+if (sacolas[4].disponibilidade <= 0) {
+    sacolas[4].fruta = false
 }
-
-
 
 function proCarrinho (){
 
@@ -156,49 +169,48 @@ function proCarrinho (){
     }
     return praSacola
 }
-    let praSacola = []
+let praSacola = []
 
-    function paraEscolher (){
+function paraEscolher (){
     let praPergunta = []
 
-if (sacolas[0].disponibilidade === true){
-    praPergunta.push(`\n maça ${sacolas[0].preco}R$`)
-}
-if (sacolas[1].disponibilidade === true){
-    praPergunta.push(`\n uva ${sacolas[1].preco}R$`)
-}
-if (sacolas[2].disponibilidade === true){
-    praPergunta.push(`\n laranja ${sacolas[2].preco}R$`)
-}
-if (sacolas[3].disponibilidade === true){
-    praPergunta.push(`\n pera ${sacolas[3].preco}R$`)
-}
-if (sacolas[4].disponibilidade === true){
-    praPergunta.push(`\n limão ${sacolas[4].preco}R$`)
-}
- return praPergunta
+    if (sacolas[0].disponibilidade > 0){
+        praPergunta.push(`\n [${sacolas[0].disponibilidade}] maça ${sacolas[0].preco}R$`)
+    }
+    if (sacolas[1].disponibilidade > 0){
+        praPergunta.push(`\n [${sacolas[1].disponibilidade}] uva ${sacolas[1].preco}R$`)
+    }
+    if (sacolas[2].disponibilidade > 0){
+        praPergunta.push(`\n [${sacolas[2].disponibilidade}] laranja ${sacolas[2].preco}R$`)
+    }
+    if (sacolas[3].disponibilidade > 0){
+        praPergunta.push(`\n [${sacolas[3].disponibilidade}] pera ${sacolas[3].preco}R$`)
+    }
+    if (sacolas[4].disponibilidade > 0){
+        praPergunta.push(`\n [${sacolas[4].disponibilidade}] limão ${sacolas[4].preco}R$`)
+    }
+    return praPergunta
 
 }
 
 let escolha = prompt(`Diga quais as frutas que você quer: ${paraEscolher()}`)
 
-let quantidades = {}
-
 if (escolha.includes(sacolas[0].fruta)){
-    quantidades.macas = (Number(prompt("Quantas maças?"))) 
+    quantidades.macas = quantidades.macas + Number(prompt("Quantas maças?"))
 } 
 if (escolha.includes(sacolas[1].fruta)){
-    quantidades.uvas = (Number(prompt("Quantas uvas?")))
+    quantidades.uvas = quantidades.uvas + Number(prompt("Quantas uvas?"))
 }
 if (escolha.includes(sacolas[2].fruta)){
-    quantidades.laranjas = (Number(prompt("Quantas laranjas?")))
+    quantidades.laranjas = quantidades.laranjas + Number(prompt("Quantas laranjas?"))
 } 
 if (escolha.includes(sacolas[3].fruta)){
-    quantidades.peras = (Number(prompt("Quantas peras?")))
+    quantidades.peras = quantidades.peras + Number(prompt("Quantas peras?"))
 }
 if (escolha.includes(sacolas[4].fruta)){
-    quantidades.limoes = (Number(prompt("Quantos limões?")))
+    quantidades.limoes = quantidades.limoes + Number(prompt("Quantos limões?"))
 }
+
 
 function numerosDinheiro (){
 
@@ -234,8 +246,6 @@ function numerosDinheiro (){
     return calculoFrutas
 }
     
-
-
 function calculoDoDinheiro (){
 
     let resultadoDoDinheiro = carteira.dinhero - numerosDinheiro()
@@ -250,9 +260,9 @@ function calculoDoDinheiro (){
         pagamento = `faltam ${-(resultadoDoDinheiro)}R$ a serem pagos`
     }
     return pagamento
-    }
-gardarDinheiro = carteira.dinhero - numerosDinheiro()
+}
 
+gardarDinheiro = carteira.dinhero - numerosDinheiro()
 
 console.log(`Itens: ${proCarrinho()} 
 \nValor: ${numerosDinheiro()}R$
